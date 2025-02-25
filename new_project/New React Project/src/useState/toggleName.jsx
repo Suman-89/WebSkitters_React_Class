7601b5df45af4dd78041f919bcc0c2bf1852fc14
Suman <suman.mukherjee01@gmail.com>
2025-02-25 11:13:56 +0530

    style added

--- new_project/New React Project/src/useState/toggleName.jsx
+++ new_project/New React Project/src/useState/toggleName.jsx
@@ -3,30 +3,45 @@ import React, { useState } from "react";
 
 export default function ToggleName() {
 
-  let myStyle = {
-    color : 'red',
-    border : '1px solid red'
-  }
-
   const [username, setUsername] = useState("Nill");
+  const [colorStyle,setColorStyle] = useState({
+    color : 'blue',
+    border : '1px solid blue',
+    borderRadius:'3px',
+    background:'yellow'
+  })
 
   const ToggleName = () => {
     console.log(username);
-    if(username=='Nill') setUsername('Pronoy');
-    else setUsername('Nill');
+    if(username=='Nill') {
+      setUsername('Pronoy');
+      setColorStyle({color : 'yellow',
+        border : '1px solid yellow',
+        borderRadius:'3px',
+        background:'green'
+      })
+    } 
+    else {
+      setUsername('Nill');
+      setColorStyle({color : 'blue',
+        border : '1px solid blue',
+        borderRadius:'3px',
+        background:'yellow'
+      })
+    }
   }
   return (
     <>
       <div className="outerbox">
-        <h1>Toggle name Project</h1>
+        <h2>Toggle name Project</h2>
         <div className="username">
-          <h3><span>Name = </span>{username}</h3>
-        </div>
+          <h4 style={colorStyle}>{username}</h4>
+        </div>&nbsp;&nbsp;
         <div className="button">
           <button
             type="button" 
             onClick={ToggleName}
-            style={myStyle}
+            style={colorStyle}
           >
             Toggle Name
           </button>
