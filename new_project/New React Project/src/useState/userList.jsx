0dd27eef54760971a268c777aaa95af186112d9d
Suman-89 <suman.mukherjee01@gmail.com>
2025-03-04 00:46:47 +0530

    onchange_final

--- new_project/New React Project/src/useState/userList.jsx
+++ new_project/New React Project/src/useState/userList.jsx
@@ -1,20 +1,57 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 export default function UserList() {
-    // const [userDetails, setUserDetails] = useState(
-    //     {
-    //         Name:'',
-    //         Address:'',
-    //         Phone:''
-    //     });
+  const [formData, setFormData] = useState({
+    Name: "",
+    Address: "",
+    Phone: "",
+  });
 
-    // const handleChange = (param) =>{
-        
-    //     setUserDetails()
-    // }
+  const handleChange = (param) =>{
+    param.target.value;
+
+  }
+  const handleAdd = (param) => {
+    param.target.value;
+  };
   return (
     <>
-    
+      <form action="">
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <button onClick={handleAdd}>Add</button>
+      </form>
     </>
   );
 }
