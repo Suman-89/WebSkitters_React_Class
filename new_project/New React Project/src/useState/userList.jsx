262aa05ce9e48b92df3362142bbd1aef95b4a5f6
Suman <suman.mukherjee01@gmail.com>
2025-03-07 19:02:23 +0530

    userlist partially done

--- new_project/New React Project/src/useState/userList.jsx
+++ new_project/New React Project/src/useState/userList.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 
 export default function UserList() {
-  const [formData, setFormData] = useState({});
-console.log(formData);
-  const handleChange = (param) =>{
-    setFormData((prev)=> ({...prev,[param.target.name]:param.target.value}));
-  }
-  const handleAdd = (e) => {
+  const [formData, setFormData] = useState({ uname: '', email: '', phone: '' });
+  const [viewData, setViewData] = useState([]);
 
+  const handleChange = (param) => {
+    setFormData((prev) => ({ ...prev, [param.target.name]: param.target.value }));
+  }
+  // setViewData([formData])
+  // viewData.push(formData);
+  // console.log(viewData);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+
+    let arr = [];
+    arr.push(formData);
+    localStorage.setItem('setData', JSON.stringify(arr));
+    const userData = JSON.parse(localStorage.getItem('setData'));
+    setViewData(userData);
+    // setFormData((prev) => ({ ...prev, [param.target.name]: '' }));
+
+    // setFormData();
   };
   return (
     <>
@@ -16,8 +30,8 @@ console.log(formData);
           Name:
           <input
             type="text"
-            name="name"
-            value={formData.name}
+            name="uname"
+            value={formData.uname}
             onChange={handleChange}
             required
           />
@@ -37,15 +51,24 @@ console.log(formData);
         <label>
           Phone:
           <input
-            type="number"
+            type="text"
             name="phone"
-            value={formData.age}
+            value={formData.phone}
             onChange={handleChange}
             required
           />
         </label>
         <br />
-        <button onClick={handleAdd}>Add</button>
+        <button onClick={(e) => handleAdd(e)}>Add</button>
+        {viewData.map((v, i) => {
+          return (<ul key={i}>
+            <li><div className="card" style={{ border: '1px solid blue', width: '250px', height: '150px' }}>
+              <h3>{v.uname}</h3><br />
+              <span>{v.email}</span><br />
+              <span>{v.phone}</span>
+            </div></li>
+          </ul>)
+        })}
       </form>
     </>
   );
